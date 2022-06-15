fe71c9913ad74e1377c1d69c1149e489f7c82d80
Kishan Gopal Modi <kishan.modi@sunarctechnologies.com>
2022-06-15 16:23:13 +0530

    Updated the design for the compnay listing

--- components/candidate-card/index.tsx
+++ components/candidate-card/index.tsx
@@ -1,23 +1,25 @@
 import Link from "next/link";
-import { Skeleton, Card, Avatar, Image, Tag } from "antd";
+import { Image } from "antd";
 
-const { Meta } = Card;
+import { ArrowRightOutlined } from "@ant-design/icons";
+
 import { Text } from "../../screens/styles";
 
 import styled from "styled-components";
 
 const CardLayout = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: end;
+  cursor: pointer;
+  position: relative;
 `;
 const CardContainer = styled.div`
   // display: flex;
-  flex: 1 200px;
-  border-radius: 25px;
-  background: linear-gradient(49deg, #cbc3ff24, #8675eb33);
-  flex: 0 100%;
-  box-shadow: -5px 16px 6px -1px #dbdbdb57;
-  padding: 30px 10px;
-  border: 1px solid #9d9d9d14;
+  padding: 10px;
+  border: 1px solid #3b354b;
+  box-shadow: 4px 5px 0px #ee7968;
+  flex: 0 94%;
 
   .ant-image {
     margin: 0 auto;
@@ -28,62 +30,90 @@ const CardContainer = styled.div`
   }
 `;
 const ImageCandidate = styled(Image)`
-  border-radius: 50%;
-  height: 100px;
-  width: 100px;
-  margin: 0 auto;
+  border-radius: 55px 5px 55px 5px;
+  margin: 0 auto 20px;
   display: block;
-  padding: 10px 10px;
+
+  &:nth-of-type(5) {
+    border-radius: 5px 55px 5px 55px;
+  }
+`;
+
+const RotatedTitle = styled(Text)`
+  position: absolute;
+  left: 15px;
+  bottom: 0;
+  transform: rotate(-90deg) translateZ(0);
+  transform-origin: 0 100%;
+  backface-visibility: hidden;
 `;
 
 const CandidateCard = (props) => {
   const { employee, company } = props;
   return (
     <>
-      {/* <Card>
-        <Skeleton avatar active loading={false}>
-          <Link href={`/${company.id}/${employee.id}`}>
-            <Meta
-              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title={employee.name}
-              description={employee.skill_intro}
-            />
-          </Link>
-        </Skeleton>
-      </Card> */}
-      <div>
+      <Link href={`/${company.id}/${employee.id}`}>
         <CardLayout>
+          <RotatedTitle
+            fs="20px"
+            ta="center"
+            lh="1.5"
+            fw="600"
+            color="#251b4d"
+            fl="unset"
+          >
+            {employee.name}
+          </RotatedTitle>
           <CardContainer>
             <ImageCandidate
-              style={{}}
               src="https://picsum.photos/200"
               alt={`${employee.name}'s image`}
               preview={false}
             />
             <Text
-              fs="16px"
-              ta="center"
-              lh="1.5"
-              fw="600"
-              color="#251b4d"
-              fl="unset"
-            >
-              {employee.name}
-            </Text>
-            <Text
-              fs="11px"
-              ta="center"
+              cursor="pointer"
+              fs="17px"
               lh="1.5"
               fw="400"
-              color="#827da1"
+              color="#171609"
               fl="unset"
             >
-              {employee.skill_intro}
+              - ReactJs Developer
             </Text>
-            <Tag color="#edeefd">Frontend Developer</Tag>
+            <Text
+              cursor="pointer"
+              fs="17px"
+              lh="1.5"
+              fw="400"
+              color="#171609"
+              fl="unset"
+            >
+              - Can start work within a week
+            </Text>
+            <Text
+              cursor="pointer"
+              fs="17px"
+              lh="1.5"
+              fw="400"
+              color="#171609"
+              fl="unset"
+            >
+              - {employee.skill_intro}
+            </Text>
+            <Text
+              cursor="pointer"
+              fs="17px"
+              mt="30px"
+              lh="1.5"
+              fw="400"
+              color="#171609"
+              fl="unset"
+            >
+              Read More <ArrowRightOutlined />
+            </Text>
           </CardContainer>
         </CardLayout>
-      </div>
+      </Link>
     </>
   );
 };
--- components/company-profile/index.tsx
+++ components/company-profile/index.tsx
@@ -1,9 +1,11 @@
 import { useQuery, gql } from "@apollo/client";
 import { Skeleton, Row, Col } from "antd";
 
-import Background from "../../asset/painter.svg";
+import BackgroundImageLanding from "../../asset/land-bg.jpg";
 import CandidateCard from "../../components/candidate-card";
 import styled from "styled-components";
+
+import { Text } from "../../screens/styles";
 
 const QUERY = gql`
   query company($id: ID) {
@@ -19,22 +21,17 @@ const QUERY = gql`
   }
 `;
 
-const CompanyContainer = styled.div``;
-
-const LeftContainer = styled.div`
-  background: #6c0487;
-  position: fixed;
-  top: 0;
-  height: 100vh;
-  width: 250px;
-  background: url(${Background.src}) #ff127f;
-  background-position: center center;
-  background-size: auto 140%;
+const CompanyContainer = styled.div`
+  width: 1920px;
+  margin: 0 auto;
+  max-width: 100%;
 `;
 
 const RightContainer = styled.div`
   width: 100%;
-  padding: 10px 10px 10px 260px;
+  padding: 10px;
+  width: 900px;
+  margin: 0 auto;
 `;
 
 const SkeletonLoader = styled(Skeleton)`
@@ -44,13 +41,17 @@ const SkeletonLoader = styled(Skeleton)`
   justify-content: center;
   max-width: 100%;
   position: fixed;
-  z-index: 61;
+  z-index: 2;
   width: 100%;
   margin: 0 auto;
 
   .ant-skeleton-content {
     width: 30%;
   }
+`;
+const CompanyProfileContainer = styled.div`
+  background: url(${BackgroundImageLanding.src}) #f5ece7;
+  padding: 10px 10px 100px;
 `;
 
 const CompanyProfile = ({ companyId }: { companyId: string }) => {
@@ -59,7 +60,7 @@ const CompanyProfile = ({ companyId }: { companyId: string }) => {
   });
 
   if (loading) {
-    return <SkeletonLoader />;
+    return <SkeletonLoader active />;
   }
 
   if (error) {
@@ -70,21 +71,46 @@ const CompanyProfile = ({ companyId }: { companyId: string }) => {
   const company = data.company;
 
   return (
-    <div className={"companyProfile"}>
+    <CompanyProfileContainer>
       <CompanyContainer>
-        <LeftContainer></LeftContainer>
         <RightContainer>
-          <h1> {company.name}</h1>
+          <Text
+            style={{
+              fontFamily: "var(--front-primary-font)",
+            }}
+            mb="30px"
+            fl="unset"
+            lh="1.5"
+            color="#EE7968"
+            fs="79px"
+          >
+            {company.name}
+          </Text>
+          <Text
+            style={{
+              fontFamily: "var(--front-primary-font)",
+            }}
+            mb="55px"
+            fl="unset"
+            lh="1.5"
+            color="#3F1A18"
+            fs="19px"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris.
+          </Text>
           <div className="list_of_employees">
             {company.employees.length != 0 ? (
-              <Row gutter={16}>
+              <Row gutter={[30, 30]}>
                 {company.employees.map((employee) => {
                   return (
                     <Col
                       xs={{ span: 24 }}
                       sm={{ span: 12 }}
-                      md={{ span: 8 }}
-                      lg={{ span: 6 }}
+                      lg={{ span: 8 }}
                       key={employee.id}
                     >
                       <CandidateCard company={company} employee={employee} />
@@ -96,9 +122,88 @@ const CompanyProfile = ({ companyId }: { companyId: string }) => {
               "No Candidate is assigned to this company"
             )}
           </div>
+
+          <Text
+            style={{
+              fontFamily: "var(--front-primary-font)",
+            }}
+            mt="50px"
+            mb="10px"
+            fl="unset"
+            lh="1.5"
+            color="#3F1A18"
+            fs="19px"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris. ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris.
+          </Text>
+          <Text
+            style={{
+              fontFamily: "var(--front-primary-font)",
+            }}
+            mb="10px"
+            fl="unset"
+            lh="1.5"
+            color="#3F1A18"
+            fs="19px"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris.
+          </Text>
+          <Text
+            style={{
+              fontFamily: "var(--front-primary-font)",
+            }}
+            mb="10px"
+            fl="unset"
+            lh="1.5"
+            color="#3F1A18"
+            fs="19px"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris.
+          </Text>
+
+          <iframe
+            width="100%"
+            height="415"
+            style={{ margin: "10px 0px" }}
+            src="https://www.youtube.com/embed/a5INJ6dy6pc"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <Text
+            style={{
+              fontFamily: "var(--front-primary-font)",
+            }}
+            mb="55px"
+            fl="unset"
+            lh="1.5"
+            color="#3F1A18"
+            fs="19px"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris.
+          </Text>
         </RightContainer>
       </CompanyContainer>
-    </div>
+    </CompanyProfileContainer>
   );
 };
 
